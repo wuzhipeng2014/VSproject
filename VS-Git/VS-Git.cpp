948c7b65f60ac87bad7e753c45e0e131db932208
WuZhipeng <798288416@qq.com>
2015-01-13 11:34:52 +0800

    init

---
+++ VS-Git/VS-Git.cpp
@@ -0,0 +1,14 @@
+// VS-Git.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
